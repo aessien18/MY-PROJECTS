8e2b7e07520d30ec1cb48b16eb18aea130b17bd7
aessein18 <angelaessein@gmail.com>
2026-03-15 19:34:54 +0000

    Added average grade

--- GradeChecker.py
+++ GradeChecker.py
@@ -1,13 +1,26 @@
-score = int(input("Enter your score:"))
-if score >=90 and score <=100:
-    print("Grade:A")
-elif score >=80 and score <90:
-    print("Grade:B")
-elif score >=70 and score <80:
-    print("Grade:C")
-elif score >=60 and score <70:
-    print("Grade:D")
-elif score >=0 and score <60: 
-    print("Grade:E")
-elif score == 0 or score < 50:
-    print("Grade:F")
+scores = list(map(float, input("Enter your score: ").split()))
+
+scores =[score for score in scores ]
+total = sum(scores)
+count = len(scores)
+average = total / count if count > 0 else 0
+
+def calculate_grade(score):
+    if 70 <= score <= 100:
+        return "A"
+    elif 60 <= score < 70:   # 60–69
+        return "B"
+    elif 50 <= score < 60:   # 50–59
+        return "C"
+    elif 40 <= score < 50:   # 40–49
+        return "D"
+    else:                    # Anything below 40
+        return "F"
+
+print("\nGrades for each score:")
+for score in scores:
+    print(f"Score: {score} -> Grade:{calculate_grade(score)}")
+print(f"\nTotal: {total}")
+print(f"Count: {count}")
+print(f"Average: {average:.2f}")
+print(f"Average Grade:{calculate_grade(average)}")
