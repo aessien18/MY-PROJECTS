77989f0f8eb6136d8619cc04c62ea7275497f4f2
aessein18 <angelaessein@gmail.com>
2026-03-15 19:58:48 +0000

    added feedback

--- GradeChecker.py
+++ GradeChecker.py
@@ -7,20 +7,25 @@ average = total / count if count > 0 else 0
 
 def calculate_grade(score):
     if 70 <= score <= 100:
-        return "A"
+        return "A", "Excellent"
     elif 60 <= score < 70:   # 60–69
-        return "B"
+        return "B", "Good"
     elif 50 <= score < 60:   # 50–59
-        return "C"
+        return "C", "Satisfactory"
     elif 40 <= score < 50:   # 40–49
-        return "D"
+        return "D", "Unsatisfactory"
     else:                    # Anything below 40
-        return "F"
+        return "F", "Fail"
+    
+    
 
 print("\nGrades for each score:")
 for score in scores:
-    print(f"Score: {score} -> Grade:{calculate_grade(score)}")
+    grade ,feedback = calculate_grade(score)
+    print(f"Score: {score} -> Grade:{grade} ({feedback})")
+    
+    avg_grade, avg_feedback = calculate_grade(average)
 print(f"\nTotal: {total}")
 print(f"Count: {count}")
 print(f"Average: {average:.2f}")
-print(f"Average Grade:{calculate_grade(average)}")
+print(f"Average Grade:{avg_grade} ({avg_feedback})")
