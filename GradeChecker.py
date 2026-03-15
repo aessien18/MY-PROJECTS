0f71ff4fcd0228d2e0fc7cb485e9a5a288f88d65
aessein18 <angelaessein@gmail.com>
2026-03-15 16:36:09 +0000

    Intial commit

---
+++ GradeChecker.py
@@ -0,0 +1,13 @@
+score = int(input("Enter your score:"))
+if score >=90 and score <=100:
+    print("Grade:A")
+elif score >=80 and score <90:
+    print("Grade:B")
+elif score >=70 and score <80:
+    print("Grade:C")
+elif score >=60 and score <70:
+    print("Grade:D")
+elif score >=0 and score <60: 
+    print("Grade:E")
+elif score == 0 or score < 50:
+    print("Grade:F")
